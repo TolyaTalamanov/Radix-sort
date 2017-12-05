86088815a0e914804836c36ec5fa9b207b299641
tolik <talamtolik@gmail.com>
2017-12-05 14:33:31 +0300

    new sorts

--- include/Sorts.h
+++ include/Sorts.h
@@ -5,6 +5,8 @@
 #include <iterator>
 #include <algorithm>
 #include <chrono>
+#include <queue>
+
 using Time = std::chrono::high_resolution_clock;
 using ms   = std::chrono::milliseconds;
 using fms  = std::chrono::duration<float, std::milli>;
@@ -24,12 +26,40 @@ public:
 };
 
 template<typename Iterator>
-void lsd_radix_sort(Iterator first, Iterator last)
+void lsd_radix_sort_bits(Iterator first, Iterator last)
 {
     for (int lsb = 0; lsb < 32; ++lsb)
     {
         std::stable_partition(first, last, radix_compare(lsb));
     }
+}
+template<typename Iterator>
+void lsd_radix_sort(Iterator first, Iterator last){
+    const int n = std::distance(first, last);
+    const int counts_bucket = 10;
+    std::queue<int> buckets[counts_bucket];
+    int bucket_index;
+    int max = *std::max_element(first, last);
+    int size_bucket;
+    for(int power = 1; max != 0; power *= counts_bucket, max /= counts_bucket ){
+        for(auto it = first; it != last; ++it){
+            bucket_index = (*it / power) % counts_bucket;
+            buckets[bucket_index].push(*it);
+        }
+        Iterator it = first;
+        for(auto& bucket : buckets){
+            size_bucket = bucket.size();
+            for(int i = 0 ; i < size_bucket; ++i){
+                *it = bucket.front();
+                bucket.pop();
+                it++;
+            }
+        }
+        std::cout << "\niteration : " <<power / counts_bucket << '\n';
+        std::copy(first, last, std::ostream_iterator<int>(std::cout, " "));
+        std::cout << "" << '\n';
+    }
+
 }
 
 #endif //MPI_LAB_3_RADIX_SORT_INCLUDE_SORTS_H
